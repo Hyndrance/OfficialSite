5ed3af434695f54d43e826e0716c9569108df9b2
Robert <robertcdc21@gmail.com>
2016-12-19 14:50:43 +0800

    Changed services area

--- services.php
+++ services.php
@@ -13,27 +13,35 @@
             <div class="space"></div>
 
             <div class="row">
-                <div class="col-md-4 col-sm-4">
+				<div class="col-xs-3 col-sm-3">
                     <div class="service">
-                        <span class="fa fa-book fa-3x"></span>
+                        <span class="fa fa-desktop fa-3x"></span>
                         <h4>Website Design</h4>
                         <p>The different areas of web design include web graphic design; interface design; authoring, including standardised code and proprietary software; user experience design; and search engine optimization. </p>
                     </div>
                 </div>
 
-                <div class="col-md-4 col-sm-4">
+                <div class="col-xs-3 col-sm-3">
                     <div class="service">
-                        <span class="fa fa-cog fa-3x"></span>
+                        <span class="fa fa-gears fa-3x"></span>
                         <h4>App Development</h4>
                         <p>These applications can be pre-installed on phones during manufacturing platforms, or delivered as web applications using server-side or client-side processing (e.g. JavaScript) to provide an "application-like" experience within a Web browser. </p>
                     </div>
                 </div>
 
-                <div class="col-md-4 col-sm-4">
+                <div class="col-xs-3 col-sm-3">
                     <div class="service">
                         <span class="fa fa-paint-brush fa-3x"></span>
                         <h4>Graphic Design</h4>
                         <p>We can design what our customer wants. We are capable of designing projects at a very good quality because of our skills, and creativity. </p>
+                    </div>
+                </div>
+				
+				<div class="col-xs-3 col-sm-3">
+                    <div class="service">
+                        <span class="fa fa-paint-brush fa-3x"></span>
+                        <h4>Ecommerce</h4>
+                        <p>These business transactions occur either business-to-business, business-to-consumer, consumer-to-consumer or consumer-to-business.</p>
                     </div>
                 </div>
             </div>
