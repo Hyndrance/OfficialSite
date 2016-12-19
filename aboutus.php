b07b81eb6f6445b8c0003160d2787d0f1f54bf8c
agentyellow66 <johnlovaldez22@gmail.com>
2016-12-19 14:51:36 +0800

    Updating the content of the About Us

--- aboutus.php
+++ aboutus.php
@@ -6,7 +6,7 @@
                         <h2>About Us</h2>
                         <hr>
                     </div>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    <p>IPNOLOGY provides and develop softwares for personal use or businesses. We are programmers who are both creative and technically inclined, and use both these attributes to build or redesign websites. We also have the ability to understand what is needed to make a website functional and easy to use, but at the same time make it aesthetically appealing to the user.</p>
                 </div>
             </div>
             <div class="space"></div>
@@ -15,38 +15,68 @@
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
                             <div class="team">
-                                <img src="include/img/team/02.jpg" class="img-responsive img-circle" alt="...">
+                                <img src="include/img/team/fred.jpg" class="img-responsive img-circle" alt="...">
                                 <br>
-                                <h4>Lian Gwapa</h4>
-                                <p class="small">CEO/Founder</p>
+                                <h4>Winifred Garcia "Fred"</h4>
+                                <p class="small">Project Manager/CEO/Founder</p>
                                 <hr>
                             </div>
                         </div>
 
                         <div class="col-md-4 col-sm-4">
                             <div class="team">
-                                <img src="include/img/team/04.jpg" class="img-responsive img-circle" alt="...">
+                                <img src="include/img/team/den.jpg" class="img-responsive img-circle" alt="...">
                                 <br>
-                                <h4>Jenn Pereira</h4>
-                                <p class="small">Web Coder</p>
+                                <h4>Denmark Contrevida </br>"Den"</h4>
+                                <p class="small">Server Administrator</p>
                                 <hr>
                             </div>
                         </div>
 
                         <div class="col-md-4 col-sm-4">
                             <div class="team">
-                                <img src="include/img/team/03.jpg" class="img-responsive img-circle" alt="...">
+                                <img src="include/img/team/debbie.jpg" class="img-responsive img-circle" alt="...">
                                 <br>
-                                <h4>Alan Podemski</h4>
-                                <p class="small">Web Designer</p>
+                                <h4>Debbie Severino "Deb"</h4>
+                                <p class="small">System Analyst</p>
+                                <hr>
+                            </div>
+                        </div>
+					
+					<div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/dale.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>Dale Torre </br>"Dale"</h4>
+                                <p class="small">Backend Developer</p>
                                 <hr>
                             </div>
                         </div>
 
-                    </div>
-                </div>
+                        <div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/jl.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>John Lorenz Tajonera</br>"JL"</h4>
+                                <p class="small">Frontend Developer</p>
+                                <hr>
+                            </div>
+                        </div>
 
-                <div class="col-md-2 col-sm-2">
+                        <div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/robert.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>Robert Capapas "Ski"</h4>
+                                <p class="small">Senior Designer</p>
+                                <hr>
+                            </div>
+                        </div>
+                 
+				</div>
+				</div>
+				
+	            <!-- <div class="col-md-3 col-sm-3">
                     <div class="team hire">
                         <h4>We Are Hiring!</h4>
                         <hr>
@@ -56,13 +86,13 @@
                         </a>
                     </div>
                 </div>
-            </div>
-
+				
+				--->
+	</div>
             <div class="text-center">
                 <a href="#contact" class="page-scroll down-btn">
                     <span class="fa fa-angle-down"></span>
                 </a>
             </div>
 
-        </div>
     </div>
