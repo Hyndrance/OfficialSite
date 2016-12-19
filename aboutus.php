1cbf021f03b2299783c0b6dc451dfd7f068322ff
ciagar23 <fredowinz23@gmail.com>
2016-12-19 13:32:39 +0800

    Fixed html to php

---
+++ aboutus.php
@@ -0,0 +1,68 @@
+<div id="about-us">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="section-title">
+                        <h2>About Us</h2>
+                        <hr>
+                    </div>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                </div>
+            </div>
+            <div class="space"></div>
+            <div class="row">
+                <div class="col-md-10 col-sm-10">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/02.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>Lian Gwapa</h4>
+                                <p class="small">CEO/Founder</p>
+                                <hr>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/04.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>Jenn Pereira</h4>
+                                <p class="small">Web Coder</p>
+                                <hr>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-4">
+                            <div class="team">
+                                <img src="include/img/team/03.jpg" class="img-responsive img-circle" alt="...">
+                                <br>
+                                <h4>Alan Podemski</h4>
+                                <p class="small">Web Designer</p>
+                                <hr>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-2">
+                    <div class="team hire">
+                        <h4>We Are Hiring!</h4>
+                        <hr>
+                        <div class="space"></div>
+                        <a href="#">
+                            <span class="fa fa-paper-plane-o fa-2x"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <a href="#contact" class="page-scroll down-btn">
+                    <span class="fa fa-angle-down"></span>
+                </a>
+            </div>
+
+        </div>
+    </div>
